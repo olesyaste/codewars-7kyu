0a61a637a2e72f5321a88c7baae9dff08b6e642e
Olesya Steinmetz <newsabome@icloud.com>
2020-12-05 23:54:47 +0100

    solved

---
+++ valid-spacing.js
@@ -0,0 +1,30 @@
+function validSpacing(s) {
+    let splitted = s.split(' ')
+    let result;
+    if(s === ''){
+      return true
+    } else {
+  for(let i = 0; i < splitted.length; i++){
+    if(splitted[i] === '' || splitted[0] === '' || splitted[splitted.length-1] === ''){
+     result = false
+   } else {
+     result = true
+   }
+    }
+  }
+    return result
+  }
+  
+  
+  validSpacing('Hello world') //= true
+  validSpacing(' Hello world') //= false
+  validSpacing('Hello world  ') //= false
+  validSpacing('Hello  world') //= false
+  validSpacing('Hello') //= true
+  // Even though there are no spaces, it is still valid because none are needed
+  validSpacing('Helloworld') //= true 
+  // true because we are not checking for the validity of words.
+  validSpacing('Helloworld ') //= false
+  validSpacing(' ') //= false
+  validSpacing('')// = true
+  
